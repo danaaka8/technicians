92f76734e5353e0f4b421b0e1afdc2a3768ec008
ali99944 <ali99944@github.com>
2023-06-14 21:53:14 +0300

    update technicians controllers

--- controllers/technicianController.js
+++ controllers/technicianController.js
@@ -5,8 +5,14 @@ const createTechnician = async (req, res) => {
   try {
     const { image, name, email, phone, location, category } = req.body;
 
+    // Check if email already exists
+    const existingTechnician = await Technician.findOne({ email: email });
+    if (existingTechnician) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
     // Create a new technician instance
-    const newTechnician = new Technician({ image, name, email, phone, location, category, rating:0, numServicesDone: 0});
+    const newTechnician = new Technician({ image, name, email, phone, location, category, rating: 0, numServicesDone: 0 });
 
     // Save the technician to the database
     const savedTechnician = await newTechnician.save();
@@ -49,9 +55,15 @@ const updateTechnician = async (req, res) => {
     const { id } = req.params;
     const { image, name, email, phone, location, category } = req.body;
 
+    // Check if the new email is already taken by another technician
+    const existingTechnician = await Technician.findOne({ email: email, _id: { $ne: id } });
+    if (existingTechnician) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
     const updatedTechnician = await Technician.findByIdAndUpdate(
       id,
-      { image, name, email, phone, location, category },
+      { image, name, phone, location, category },
       { new: true }
     );
 

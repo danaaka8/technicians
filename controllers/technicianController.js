a063312104b5f5da8197f5e24c4d67a34e8a703f
ali99944 <ali99944@github.com>
2023-06-14 21:55:09 +0300

    update technicians controllers

--- controllers/technicianController.js
+++ controllers/technicianController.js
@@ -22,6 +22,66 @@ const createTechnician = async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 };
+In the updated code, we added a check to see if there is an existing technician with the provided email. If such a technician exists, we return a 400 Bad Request response with an appropriate error message indicating that the email already exists. This prevents the creation of a technician with a duplicated email.
+
+
+
+
+
+  User
+const updateTechnician = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { image, name, email, phone, location, category } = req.body;
+
+    const updatedTechnician = await Technician.findByIdAndUpdate(
+      id,
+      { image, name, email, phone, location, category },
+      { new: true }
+    );
+
+    if (!updatedTechnician) {
+      return res.status(404).json({ error: 'Technician not found' });
+    }
+
+    res.json(updatedTechnician);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+don't update the new email if the new email is already taken by someone and add a custom response for that case
+ChatGPT
+To prevent updating the email of a technician if the new email is already taken by someone else, you can modify the updateTechnician method as follows:
+
+  javascript
+Copy code
+const updateTechnician = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { image, name, email, phone, location, category } = req.body;
+
+    // Check if the new email is already taken by another technician
+    const existingTechnician = await Technician.findOne({ email: email, _id: { $ne: id } });
+    if (existingTechnician) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    const updatedTechnician = await Technician.findByIdAndUpdate(
+      id,
+      { image, name, phone, location, category },
+      { new: true }
+    );
+
+    if (!updatedTechnician) {
+      return res.status(404).json({ error: 'Technician not found' });
+    }
+
+    res.json(updatedTechnician);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
 
 // Get all technicians
 const getAllTechnicians = async (req, res) => {
